30ec33427874a23e85a97b9dc056e056aad4bf5a
Hanna Mohamed <hanna@MacBook-Pro-de-Hanna.local>
2019-10-24 10:07:27 +0200

    not finished

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,46 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
-}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 1;
+  }
+
+  startClick() {
+    let a = this;
+    this.intervalId = setInterval(function () {
+      return a.currentTime += 1;
+    }, 1000);
+  }
+
+  getMinutes() {
+    if (this.currentTime >= 60) {
+      return this.currentTime = Math.floor(this.currentTime / 60);
+    }
+    return 0;
+  }
+
+  getSeconds() {
+    if (this.currentTime > 60) {
+      return this.currentTime = this.currentTime % 60;
+    }
+    return this.currentTime;
+  }
+
+  twoDigitsNumber(value) {
+    if (value < 10) {
+      return `0` + `${value}`;
+    }
+    return `${value}`;
+  }
+
+  resetClick() {
+    this.currentTime = 0;
+  }
+
+  stopClick() {
+    clearInterval()
+  }
+  
+  splitClick() {}
+};
+
+
